8f16de9adc1a4999e9d648a9f154271de6530f81
Cédric Phung <cedric3011@outlook.fr>
2025-03-05 17:02:00 +0100

    add page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Questions from "./pages/Questions";
+import Mission from "./pages/Mission";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <Questions />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/ma-mission" element={<Mission />} />
+        <Route path="/questions-frequentes" element={<Questions />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
