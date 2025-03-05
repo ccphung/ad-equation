dc8b4ddda8631e38b52d977b5a729c9c6a6b3a94
Cédric Phung <cedric3011@outlook.fr>
2025-03-05 18:58:35 +0100

    add animation with framer motion

--- src/components/ProcessItem.jsx
+++ src/components/ProcessItem.jsx
@@ -1,12 +1,41 @@
+import { useEffect, useRef } from "react";
+import { useAnimation, useInView } from "framer-motion";
+import { motion } from "framer-motion";
+
 function ProcessItem({ file, title, desc }) {
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView, mainControls]);
+
   return (
-    <div className="m-8 flex justify-center align-middle">
-      <img className="w-16" src={`/images/icons/${file}.svg`} alt="handshake" />
-      <div className="ml-4">
-        <h5 className="font-bold text-stone-600">{title}</h5>
-        <p className="text-stone-500">{desc}</p>
-      </div>
-    </div>
+    <motion.div className="m-8 " ref={containerRef}>
+      <motion.div
+        className="flex justify-center items-center"
+        initial="hidden"
+        animate={mainControls}
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+      >
+        <img
+          className="w-16"
+          src={`/images/icons/${file}.svg`}
+          alt="handshake"
+        />
+        <div className="ml-4">
+          <h5 className="font-bold text-stone-600">{title}</h5>
+          <p className="text-stone-500">{desc}</p>
+        </div>
+      </motion.div>
+    </motion.div>
   );
 }
 
