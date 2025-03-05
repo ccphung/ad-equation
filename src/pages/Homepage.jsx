20fde5c8b96fc63e3ee7c0cab168e2e1f9f8b8d7
Cédric Phung <cedric3011@outlook.fr>
2025-03-05 14:37:49 +0100

    create found properties section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,15 @@
+import FoundProperties from "../components/FoundProperties";
 import Hero from "../components/Hero";
 import Process from "../components/Process";
+import WhyDelegate from "../components/WhyDelegate";
 
 function Homepage() {
   return (
     <div>
       <Hero />
       <Process />
+      <WhyDelegate />
+      <FoundProperties />
     </div>
   );
 }
