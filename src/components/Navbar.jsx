5396ba15b324b6810503a90b64d702fdf7bb7bb6
Cédric Phung <cedric3011@outlook.fr>
2025-03-05 17:01:32 +0100

    modify links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Dialog, DialogPanel, PopoverGroup } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -11,13 +12,12 @@ function Navbar() {
     <header className="bg-white">
       <nav
         aria-label="Global"
-        className="mx-auto flex  items-center justify-between p-6 lg:px-8 lg:border-b border-stone-400"
+        className="mx-auto flex items-center justify-between p-6 lg:px-8 lg:border-b border-stone-400"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Ad Equation</span>
+          <Link to="/">
             <img alt="" src="/images/logo/logo.png" className="h-14 w-auto" />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -25,31 +25,62 @@ function Navbar() {
             onClick={() => setMobileMenuOpen(true)}
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
           >
-            <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "text-sm font-semibold text-indigo-400 border-b-2 border-indigo-400 g px-3 py-2"
+                : "text-sm font-semibold text-gray-900 gray-50 rounded-lg px-3 py-2 hover:bg-gray-50"
+            }
+          >
             Accueil
-          </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+          </NavLink>
+          <NavLink
+            to="/ma-mission"
+            className={({ isActive }) =>
+              isActive
+                ? "text-sm font-semibold text-indigo-400 border-b-2 border-indigo-400 g px-3 py-2"
+                : "text-sm font-semibold text-gray-900 gray-50 rounded-lg px-3 py-2 hover:bg-gray-50"
+            }
+          >
             Ma mission
-          </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+          </NavLink>
+          <NavLink
+            to="/votre-chasseur-immobilier"
+            className={({ isActive }) =>
+              isActive
+                ? "text-sm font-semibold text-indigo-400 border-b-2 border-indigo-400 g px-3 py-2"
+                : "text-sm font-semibold text-gray-900 gray-50 rounded-lg px-3 py-2 hover:bg-gray-50"
+            }
+          >
             Votre chasseur immobilier
-          </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+          </NavLink>
+          <NavLink
+            to="/questions-frequentes"
+            className={({ isActive }) =>
+              isActive
+                ? "text-sm font-semibold text-indigo-400 border-b-2 border-indigo-400 g px-3 py-2"
+                : "text-sm font-semibold text-gray-900 gray-50 rounded-lg px-3 py-2 hover:bg-gray-50"
+            }
+          >
             Questions fréquentes
-          </a>
+          </NavLink>
         </PopoverGroup>
-        <div className="hidden lg:flex  lg:justify-end lg:flex-1">
-          <a
-            href="#"
-            className="text-sm/6 font-semibold bg-black text-gray-50 px-2 py-1"
+        <div className="hidden lg:flex lg:justify-end lg:flex-1">
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive
+                ? "text-base font-semibold bg-indigo-400  text-white rounded-lg px-3 py-2 "
+                : "text-base font-semibold bg-gray-900 text-white rounded-lg px-3 py-2 hover:bg-gray-500"
+            }
           >
             Contact
-          </a>
+          </NavLink>
         </div>
       </nav>
       <Dialog
@@ -60,9 +91,6 @@ function Navbar() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Ad Equation</span>
-            </a>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -75,38 +103,58 @@ function Navbar() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-base font-semibold text-indigo-400 hover:bg-gray-50 block rounded-lg px-3 py-2"
+                      : "text-base font-semibold text-gray-900 hover:bg-gray-50 block rounded-lg px-3 py-2"
+                  }
                 >
                   Accueil
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                </NavLink>
+                <NavLink
+                  to="/ma-mission"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-base font-semibold text-indigo-400  hover:bg-gray-50 block rounded-lg px-3 py-2"
+                      : "text-base font-semibold text-gray-900 hover:bg-gray-50 block rounded-lg px-3 py-2"
+                  }
                 >
                   Ma mission
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                </NavLink>
+                <NavLink
+                  to="/votre-chasseur-immobilier"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-base font-semibold text-indigo-400   hover:bg-gray-50 block rounded-lg px-3 py-2 "
+                      : "text-base font-semibold text-gray-900 hover:bg-gray-50 block rounded-lg px-3 py-2"
+                  }
                 >
                   Votre chasseur immobilier
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                </NavLink>
+                <NavLink
+                  to="/questions-frequentes"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-base font-semibold text-indigo-400   hover:bg-gray-50 block rounded-lg px-3 py-2 "
+                      : "text-base font-semibold text-gray-900 hover:bg-gray-50 block rounded-lg px-3 py-2"
+                  }
                 >
                   Questions fréquentes
-                </a>
+                </NavLink>
               </div>
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-base font-semibold text-indigo-400   hover:bg-gray-50 block rounded-lg px-3 py-2 "
+                      : "text-base font-semibold text-gray-900 hover:bg-gray-50 block rounded-lg px-3 py-2"
+                  }
                 >
                   Contact
-                </a>
+                </NavLink>
               </div>
             </div>
           </div>
