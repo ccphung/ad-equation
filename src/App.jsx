e36a160eee9610b5287f31df89a5a183bc017efa
Cédric Phung <cedric3011@outlook.fr>
2025-03-06 21:05:24 +0100

    add error page and spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Homepage from "./pages/Homepage";
-import Questions from "./pages/Questions";
-import Mission from "./pages/Mission";
-import YourHunter from "./pages/YourHunter";
+import { lazy, Suspense } from "react";
+import Spinner from "./components/Spinner"; // Ton loader
+
+const Footer = lazy(() => import("./components/Footer"));
+const Navbar = lazy(() => import("./components/Navbar"));
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Questions = lazy(() => import("./pages/Questions"));
+const Mission = lazy(() => import("./pages/Mission"));
+const YourHunter = lazy(() => import("./pages/YourHunter"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/ma-mission" element={<Mission />} />
-        <Route path="/votre-chasseur-immobilier" element={<YourHunter />} />
-        <Route path="/questions-frequentes" element={<Questions />} />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<Spinner />}>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/ma-mission" element={<Mission />} />
+              <Route
+                path="/votre-chasseur-immobilier"
+                element={<YourHunter />}
+              />
+              <Route path="/questions-frequentes" element={<Questions />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Suspense>
     </BrowserRouter>
   );
 }
