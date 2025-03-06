46365824ed70384809081dd52e6ad515ce66c3a1
Cédric Phung <cedric3011@outlook.fr>
2025-03-06 15:35:32 +0100

    add divider

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,64 +1,24 @@
 import QuestionItem from "../components/QuetionItem";
 
+const questions = [
+  {question: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui", reponse: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"},
+  {question: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui", reponse: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"},
+  {question: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui", reponse: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"},
+  {question: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui", reponse: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"},
+  
+]
+
 function Questions() {
   return (
     <div className="flex justify-center m-5">
       <div className=" lg:w-[50%] sm:w-[80%] flex flex-col justify-start">
-        <h1 className=" text-[2.5em] text-stone-800 font-bold mb-5">
+        <h1 className=" text-[2.5em] text-stone-800 font-bold mb-5" >
           Questions fréquentes
         </h1>
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
-        <QuestionItem
-          question="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-          response="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui"
-        />
+        <div className="divide-y divide-stone-200">
+                  {questions.map((question, index) => <QuestionItem key={index} question={question.question} response={question.reponse}/>)}
+        </div>
+
       </div>
     </div>
   );
