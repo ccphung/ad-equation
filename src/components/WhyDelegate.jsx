8fc1feac522c796e766f94dee862af2d29eb116d
Cédric Phung <cedric3011@outlook.fr>
2025-03-08 16:21:27 +0100

    modify display

--- src/components/WhyDelegate.jsx
+++ src/components/WhyDelegate.jsx
@@ -3,7 +3,7 @@ import ListItem from "./ListItem";
 function WhyDelegate() {
   return (
     <div className="m-4 flex justify-center">
-      <div className="border border-red-300 b-4 rounded-lg w-[500px] p-7">
+      <div className="border-4 border-[#EF7970]  rounded-lg w-[500px] p-7">
         <h2 className="text-center text-[1.5em] text-stone-700 mb-6">
           Pourquoi déléguer ma recherche ?
         </h2>
